86f09927549ee201dc430da7a4df83123c38c06a
Methal Abeywickrama <methal.abeywickrama@icloud.com>
2025-12-14 19:06:44 +0530

    Change file structure
    extracted the maze controller and worldfile into the standard file
    structure

---
+++ controllers/maze_runner/build/release/maze_runner.d
@@ -0,0 +1,14 @@
+build/release/maze_runner.o: maze_runner.cpp explorer.h motion_control.h \
+  sensing.h \
+  /Applications/Webots.app/Contents/include/controller/cpp/webots/Camera.hpp \
+  /Applications/Webots.app/Contents/include/controller/cpp/webots/Device.hpp \
+  /Applications/Webots.app/Contents/include/controller/cpp/webots/../../c/webots/types.h \
+  /Applications/Webots.app/Contents/include/controller/cpp/webots/../../c/webots/camera_recognition_object.h \
+  /Applications/Webots.app/Contents/include/controller/cpp/webots/DistanceSensor.hpp \
+  /Applications/Webots.app/Contents/include/controller/cpp/webots/Gyro.hpp \
+  /Applications/Webots.app/Contents/include/controller/cpp/webots/LED.hpp \
+  /Applications/Webots.app/Contents/include/controller/cpp/webots/LightSensor.hpp \
+  /Applications/Webots.app/Contents/include/controller/cpp/webots/PositionSensor.hpp \
+  /Applications/Webots.app/Contents/include/controller/cpp/webots/Robot.hpp \
+  /Applications/Webots.app/Contents/include/controller/cpp/webots/Motor.hpp \
+  navigator.h methal_sensing.h
